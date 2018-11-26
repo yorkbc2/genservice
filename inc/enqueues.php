beeadc6c5264c98d7084a8b8a96803fb67417cc6
Kudinov Fedor <brothersrabbits@mail.ru>
2018-11-26 10:07:25 +0200

    Add to customizer settigs Work Schedule;

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -26,7 +26,7 @@ function bw_enqueues()
         wp_enqueue_script('slick');
     }
 
-    if ( ! WP_DEBUG) {
+    if (!WP_DEBUG) {
         wp_deregister_script('jquery');
         wp_register_script('jquery', get_template_directory_uri() . '/assets/js/jquery-1.12.4.min.js', array(), null,
             true);
