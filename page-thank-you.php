0d4484a44f911148f279a2e9b9bc7e2ee991aa2e
YegorMamay <web@brainworks.com.ua>
2019-01-19 20:52:16 +0200

    Thank you page corrections

--- page-thank-you.php
+++ page-thank-you.php
@@ -4,18 +4,25 @@
  **/
 ?>
 
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+    
+	<title><?php wp_title(); ?></title>
+	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/favicon.ico" type="image/x-icon" />
 
-<?php if (have_posts()): while (have_posts()): the_post(); ?>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
 
-    <h1 class="page-name"><?php the_title() ?></h1>
+<?php get_template_part('loops/content', 'page'); ?>
 
-    <?php the_content() ?>
-    <?php wp_link_pages(); ?>
+<?php wp_footer(); ?>
 
-<?php endwhile;
-else: ?>
-    <?php get_template_part('loops/content', 'none'); ?>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+</body>
+</html>
